e99ad931fb9009f70bfd69416a70950c5113d9f3
Benjamin Geiger <mail@benjamingeiger.net>
2024-11-21 17:50:12 +0100

    add pcm_gpio components

---
+++ esphome/components/pcm5122/pcm_gpio.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "esphome/core/gpio.h"
+
+#include "pcm5122.h"
+
+namespace esphome {
+namespace pcm5122 {
+
+class PCMGPIOPin : public GPIOPin {
+public:
+  void setup() override {};
+  void pin_mode(gpio::Flags flags) override {}
+  bool digital_read() override;
+  void digital_write(bool value) override;
+  std::string dump_summary() const override { return ""; };
+
+  void set_pin(XMOSPort port, uint8_t pin) {
+    this->port_ = port;
+    this->pin_ = pin;
+  }
+  void set_inverted(bool inverted) { this->inverted_ = inverted; }
+  void set_flags(gpio::Flags flags) { this->flags_ = flags; }
+
+protected:
+  XMOSPort port_;
+  uint8_t pin_;
+  bool inverted_;
+  gpio::Flags flags_;
+};
+
+} // namespace pcm5122
+} // namespace esphome
