14979033d3c0b773fe2d20c69d9c3abd3270253d
Mischa Siekmann <mischa@gnumpi.com>
2024-12-18 19:19:08 +0100

    volume and mute support for tas2780

--- esphome/components/tas2780/tas2780.cpp
+++ esphome/components/tas2780/tas2780.cpp
@@ -11,6 +11,7 @@ static const char *const TAG = "tas2780";
 
 static const uint8_t TAS2780_PAGE_SELECT = 0x00;        // Page Select
 
+/* PAGE 0*/
 static const uint8_t TAS2780_MODE_CTRL = 0x02;        
 static const uint8_t TAS2780_MODE_CTRL_BOP_SRC__PVDD_UVLO  = 0x80;
 static const uint8_t TAS2780_MODE_CTRL_MODE_MASK  = 0x07;
@@ -18,12 +19,34 @@ static const uint8_t TAS2780_MODE_CTRL_MODE__ACTIVE  = 0x00;
 static const uint8_t TAS2780_MODE_CTRL_MODE__ACTIVE_MUTED  = 0x01;
 static const uint8_t TAS2780_MODE_CTRL_MODE__SFTW_SHTDWN  = 0x02;
 
+static const uint8_t TAS2780_CHNL_0 = 0x03;
+static const uint8_t TAS2780_CHNL_0_CDS_MODE_SHIFT = 6;
+static const uint8_t TAS2780_CHNL_0_CDS_MODE_MASK = (0x03 << TAS2780_CHNL_0_CDS_MODE_SHIFT);
+static const uint8_t TAS2780_CHNL_0_AMP_LEVEL_SHIFT = 1;
+static const uint8_t TAS2780_CHNL_0_AMP_LEVEL_MASK = (0x1F) << TAS2780_CHNL_0_AMP_LEVEL_SHIFT;
+
+static const uint8_t TAS2780_DC_BLK0 = 0x04;
+static const uint8_t TAS2780_DC_BLK0_VBAT1S_MODE_SHIFT  = 7;
+
+static const uint8_t TAS2780_DVC = 0x1a;
+
 static const uint8_t TAS2780_INT_MASK0 = 0x3b;
 static const uint8_t TAS2780_INT_MASK1 = 0x3c;
 static const uint8_t TAS2780_INT_MASK4 = 0x3d;
 static const uint8_t TAS2780_INT_MASK2 = 0x40;
 static const uint8_t TAS2780_INT_MASK3 = 0x41;
 
+
+
+/* PAGE 1*/
+static const uint8_t TAS2780_INT_LDO = 0x36;
+
+static const uint8_t POWER_MODES[4][2] = {
+  {2, 0}, // PWR_MODE0: CDS_MODE=10, VBAT1S_MODE=0
+  {0, 0}, // PWR_MODE1: CDS_MODE=00, VBAT1S_MODE=0
+  {3, 1}, // PWR_MODE2: CDS_MODE=11, VBAT1S_MODE=1
+  {1, 0}, // PWR_MODE3: CDS_MODE=01, VBAT1S_MODE=0
+};
 
 
 void TAS2780::setup(){
@@ -210,10 +233,35 @@ float TAS2780::volume() {
 }
 
 bool TAS2780::write_mute_() {
+  if( this->is_muted_ ){
+    this->reg(TAS2780_DVC) = 0xc9;  
+  } else {
+    this->write_volume_(); 
+  }
   return true;
 }
 
 bool TAS2780::write_volume_() {
+  /*
+  V_{AMP} = INPUT + A_{DVC} + A_{AMP}
+  
+  V_{AMP} is the amplifier output voltage in dBV ()
+  INPUT: digital input amplitude as a number of dB with respect to 0 dBFS
+  A_{DVC}: is the digital volume control setting as a number of dB (default 0 dB)
+  A_{AMP}: the amplifier output level setting as a number of dBV
+
+  AMP_LEVEL[4:0] : @48ksps 11dBV - 21dBV [0x00, 0x14]
+  */ 
+  float attenuation = (1. - this->volume_) * 200.f + .5f;
+  uint8_t dvc = clamp<uint8_t>(attenuation, 0, 0xc8);
+  this->reg(TAS2780_DVC) = dvc; 
+  
+  uint8_t amp_level = 7; // 15dBV
+  uint8_t reg_val = this->reg(TAS2780_CHNL_0).get();
+  reg_val &= ~TAS2780_CHNL_0_AMP_LEVEL_MASK;
+  reg_val |= amp_level << TAS2780_CHNL_0_AMP_LEVEL_SHIFT;
+  this->reg(TAS2780_CHNL_0) = reg_val;
+
   return true;
 }
 
