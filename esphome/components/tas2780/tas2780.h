004aa1c9bdfbd8d28bca5dfabe637216e64fdc2d
Mischa Siekmann <mischa@gnumpi.com>
2025-05-09 13:51:35 +0200

    changed default minimum of volume range to 0.3

--- esphome/components/tas2780/tas2780.h
+++ esphome/components/tas2780/tas2780.h
@@ -42,7 +42,7 @@ class TAS2780 : public audio_dac::AudioDac, public Component, public i2c::I2CDev
   float volume_{0};
   uint8_t power_mode_{2};
   uint8_t amp_level_{8};
-  float vol_range_min_{.2};
+  float vol_range_min_{.3};
   float vol_range_max_{1.};
 
 };
